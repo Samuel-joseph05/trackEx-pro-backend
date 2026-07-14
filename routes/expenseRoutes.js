049b0b08222a9ab735f7deb7fd9d50ca885baba5
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-15 00:04:42 +0530

    feat: add user registration and expense management routes

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,10 +1,19 @@
 import express from "express"
-import { expenseRegister } from "../controllers/expenseControllers.js"
+import { deleteExpense, expenseRegister, getExpense, updateExpense, userRegister } from "../controllers/expenseControllers.js"
+
 
 
 
 const router=express.Router()
 
+//expense register route
 router.post("/expenseRegister",expenseRegister)
 
-export default router
+//user register route
+router.post("/register",userRegister)
+router.get("/expenses",getExpense)
+router.delete("/expense/:id",deleteExpense)
+router.put("/expense/:id",updateExpense)
+
+
+export default router;
