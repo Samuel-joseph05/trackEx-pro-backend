804bbc9320f99c7c796e4f32bef2d64f118db91f
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-15 00:02:23 +0530

    feat: create User model with schema for name, email, phone, and password

---
+++ models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true,"Name is required"],
+    trim:true,
+    minlength:[3,"Name must be at least 3 characters"]
+  },
+  
+  email: {
+    type: String,
+    required: [true,"Email is required"],
+    unique: true,
+    lowercase:true,
+    trim:true,
+      match: [
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      "Invalid email format",
+    ],
+  },
+
+  phone: {
+    type: String,
+    required: [true,"Phone number is required"],
+    unique: true,
+  match: [/^\d{10}$/, "Phone number must be 10 digits"],
+  },
+
+  password: {
+    type: "String",
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters"],
+  },
+});
+
+export default mongoose.model("User",userSchema)
