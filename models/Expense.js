1835facb0dd478a4057e92e1a7629eeb1cf7853c
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-16 13:52:05 +0530

    style: format Expense model code for improved readability

--- models/Expense.js
+++ models/Expense.js
@@ -1,26 +1,23 @@
 import mongoose from "mongoose";
 
-const expenseSchema= new mongoose.Schema({
-    title:{
-type:String,
-required:true,
-trim:true
-    },
-    amount:{
-        type:Number,
-        required:true,
-        trim:true
-
-    },
-    category:{
-        type:String,
-        default:"Others"
-
-    },
-    date:{
-type:Date,
-default:Date.now
-    },
-    
-})
-export default mongoose.model("Expense",expenseSchema)
+const expenseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    default: "Others",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+export default mongoose.model("Expense", expenseSchema);
