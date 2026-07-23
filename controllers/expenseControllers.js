cfe472a01fa2e0f307e7fde6bd98dc47806041ed
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-24 01:47:32 +0530

    refactor: remove user registration controller from expense controllers

--- controllers/expenseControllers.js
+++ controllers/expenseControllers.js
@@ -3,7 +3,6 @@ import {
   expenseServiceRegister,
   getExpenseServive,
   updateExpenseService,
-  userServiceRegister,
 } from "../services/expenseServices.js";
 
 //expense register controller
@@ -70,49 +69,4 @@ return res.status(500).json({message:"server error",err})
   }
 }
 
-//user register controller
-export const userRegister = async (req, res) => {
-  try {
-    const { name, email, phone, password } = req.body;
 
-    // Required fields
-    if (!name || !email || !phone || !password) {
-      return res.status(400).json({
-        message: "All fields are required",
-      });
-    }
-
-    // Email validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      return res.status(400).json({
-        message: "Invalid email format",
-      });
-    }
-
-    // Password validation
-    if (password.length < 8) {
-      return res.status(400).json({
-        message: "Password must be at least 8 characters",
-      });
-    }
-
-    const newUser = await userServiceRegister({ name, email, phone, password });
-    console.log(newUser);
-    return res
-      .status(201)
-      .json({ message: "User registered successfully", user: newUser });
-  } 
-  catch (err) {
-   
-    if (
-      err.message === "Email already exists" ||
-      err.message === "Phone number already exists"
-    ) {
-      return res.status(409).json({
-        message: err.message,
-      });
-    }
-    return res.status(500).json({ message: err.message });
-  }
-};
