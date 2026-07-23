4ee0ea98ab523e2e6697c9147447338a2c64738a
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-24 01:46:23 +0530

    refactor: remove unused user registration service from expense services

--- services/expenseServices.js
+++ services/expenseServices.js
@@ -1,5 +1,4 @@
 import Expense from "../models/Expense.js";
-import User from "../models/User.js";
 
 //expense service used to register expense
 export const expenseServiceRegister = async (expenseData) => {
@@ -18,18 +17,4 @@ export const updateExpenseService=async(id,expenseData) =>{
   return await Expense.findByIdAndUpdate(id,expenseData,{new:true})
 }
 
-//user service used to register user
-export const userServiceRegister = async (userData) => {
-  const { email, phone } = userData;
-  const existingEmail = await User.findOne({ email });
-  if (existingEmail) {
-    throw new Error("Email already exists");
-  }
 
-  const existingPhone = await User.findOne({ phone });
-  if (existingPhone) {
-    throw new Error("Phone number already exists");
-  }
-
-  return await User.create(userData);
-};
