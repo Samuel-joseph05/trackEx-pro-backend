c7c5475ee4a06fc65f65faff4d1cca93fa0f2069
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-24 16:10:33 +0530

    fix: correct type declaration for password field in user schema

--- models/User.js
+++ models/User.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema({
   },
 
   password: {
-    type: "String",
+    type:String,
     required: [true, "Password is required"],
     minlength: [8, "Password must be at least 8 characters"],
   },
