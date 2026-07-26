040b299a626bc9f23512e5f4fddf36a0c7ec07c7
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-26 17:15:32 +0530

    fix: add logging for authorization header and decoded token in verifyToken middleware

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
+   console.log("Authorization Header:", authHeader);
+
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No Token provided" });
   }
@@ -11,6 +13,7 @@ export const verifyToken = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+     console.log("Decoded:", decoded);
     req.user = decoded;
     next();
 
