11bda3aa9f3715bfde118212468b6eb8afea1071
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-26 17:15:53 +0530

    fix: configure CORS to allow requests from localhost:3000 with credentials

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const app=express();
 
 
 app.use(express.json());
-app.use(cors())
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials:true
+    }
+))
 
 app.use("/api",expenseRoutes)
 app.use("/api",userRoutes)
