bcbd7759e608229e05430ae48d50d34fb04fcccf
Samuel-joseph05 <samuela05022004@gmail.com>
2026-08-14 00:57:56 +0530

    feat: implement database connection in API handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 import app from "../app.js";
+import connectDB from "../config/db.js";
 
-export default app;
+const handler = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
+
+export default handler;
